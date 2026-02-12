3e72ef0aeeac35c3c2575cfcc3dbcae6be204ee4
Joeso <avtolikcool@yandex.ru>
2026-02-12 13:09:07 +0300

    Create crmstages.php

---
+++ docker/admin_crmstages/crmstages.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * CRM Stages Admin Component for Joomla
+ * 
+ * @package     com_crmstages
+ * @version     1.0.0
+ */
+
+defined('_JEXEC') or die;
+
+// Redirect to standalone CRM interface
+header('Location: /crm-api.php');
+exit;
